2359438e8ed5646cc5a9a08f08162c60f14ba772
stormrage-web <sharfull33@gmail.com>
2024-01-10 23:46:54 +0300

    improve: added sast utils

---
+++ workspaces/sast/src/utils.js
@@ -0,0 +1,44 @@
+// Import Third-party Dependencies
+import {
+  getCallExpressionIdentifier
+} from "@nodesecure/estree-ast-utils";
+
+export function notNullOrUndefined(value) {
+  return value !== null && value !== void 0;
+}
+
+export function isUnsafeCallee(node) {
+  const identifier = getCallExpressionIdentifier(node);
+
+  // For Function we are looking for this: `Function("...")();`
+  // A double CallExpression
+  return [
+    identifier === "eval" || (identifier === "Function" && node.callee.type === "CallExpression"),
+    identifier
+  ];
+}
+
+export function rootLocation() {
+  return { start: { line: 0, column: 0 }, end: { line: 0, column: 0 } };
+}
+
+export function toArrayLocation(location = rootLocation()) {
+  const { start, end = start } = location;
+
+  return [
+    [start.line || 0, start.column || 0],
+    [end.line || 0, end.column || 0]
+  ];
+}
+
+export function extractNode(expectedType) {
+  return (callback, nodes) => {
+    const finalNodes = Array.isArray(nodes) ? nodes : [nodes];
+
+    for (const node of finalNodes) {
+      if (notNullOrUndefined(node) && node.type === expectedType) {
+        callback(node);
+      }
+    }
+  };
+}
