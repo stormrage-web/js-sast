71250b9dca2b938027b97a11fa2b316ce81203e8
stormrage-web <sharfull33@gmail.com>
2024-01-30 14:58:08 +0300

    feat: added core probes

---
+++ workspaces/sast/src/probes/isMemberExpression.js
@@ -0,0 +1,19 @@
+function validateNode(node) {
+  return [
+    node.type === "MemberExpression"
+  ];
+}
+
+function main(node, options) {
+  const { analysis } = options;
+
+  analysis.counter[node.computed ? "computedMemberExpr" : "memberExpr"]++;
+}
+
+export default {
+  name: "isMemberExpression",
+  validateNode,
+  main,
+  breakOnMatch: true,
+  breakGroup: "import"
+};
