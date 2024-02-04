13173415f42d6b78592233748a7fa54c0bb6aacc
stormrage-web <sharfull33@gmail.com>
2024-02-04 17:50:37 +0300

    feat: added directory run function

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-import { runASTAnalysis, warnings } from "./workspaces/sast/index.js";
-import { readFileSync } from "node:fs";
+import {runASTAnalysis, warnings} from "./workspaces/sast/index.js";
+import {readFileSync} from "node:fs";
 import core from '@actions/core';
-import github from '@actions/github';
+import glob from '@actions/glob';
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const patterns = ['**/**.js', '!**/node_modules/**'];
+    const globber = await glob.create(patterns.join('\n'))
+    for await (const file of globber.globGenerator()) {
+        console.log(file);
+        const {current_warnings, dependencies} = runASTAnalysis(
+            readFileSync(file, "utf-8")
+        );
+        console.log(dependencies);
+        console.dir(current_warnings, {depth: null});
+    }
+    // const readFile = util.promisify(fs.readFile)
+    // const contents = await readFile(filePath, 'utf8')
+    // core.info(`File contents:\n${contents}`)
+    // core.setOutput('contents', contents)
 } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
 }
 
-const { current_warnings, dependencies } = runASTAnalysis(
-    readFileSync("./examples/rate-map.js", "utf-8")
-);
-console.log(dependencies);
-console.dir(current_warnings, { depth: null });
+// try {
+//     // `who-to-greet` input defined in action metadata file
+//     const nameToGreet = core.getInput('who-to-greet');
+//     console.log(`Hello ${nameToGreet}!`);
+//     const time = (new Date()).toTimeString();
+//     core.setOutput("time", time);
+//     // Get the JSON webhook payload for the event that triggered the workflow
+//     const payload = JSON.stringify(github.context.payload, undefined, 2)
+//     console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//     core.setFailed(error.message);
+// }
